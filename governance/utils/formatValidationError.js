d2c787527f7922205c2ccff7bee211e19101d2be
Djdh788gshdhdhd <craighckby@gmail.com>
2026-02-11 10:24:49 +1000

    Kernel Synthesis: governance/utils/formatValidationError.js

--- governance/utils/formatValidationError.js
+++ governance/utils/formatValidationError.js
@@ -1,16 +1,12 @@
-const { format: formatSchemaError } = require('SchemaErrorFormatterTool');
-
 /**
  * Standardized function to transform validation library errors (e.g., Ajv errors) 
  * into the system's internal structured error format.
  * 
- * This function now delegates the mapping logic to the reusable SchemaErrorFormatterTool.
+ * This function delegates the mapping logic to the reusable SchemaErrorFormatterTool.
  * 
  * @param {Array<object>} rawErrors - Array of errors returned by the validation library.
  * @returns {Array<{path: string, message: string, keyword: string, params: object, schemaPath: string}>}
  */
-function formatValidationError(rawErrors = []) {
-    return formatSchemaError(rawErrors);
-}
+const { format: formatValidationError } = require('SchemaErrorFormatterTool');
 
 module.exports = { formatValidationError };
