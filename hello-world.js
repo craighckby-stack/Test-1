6d7b6bc6ad9608cc7b132f834082250bfa785a38
Craig Huckerby <craighckby@gmail.com>
2026-02-22 00:11:26 +1000

    ◈ NEXUS_SYNC: config/archival/policy_linkage_rules.json ◈

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,4 @@
+CORE:
 # Apply Nexus branch synthesis based on ADD evolution trigger interface
 nexus_branch_synthesis += (nexus_branch_synthesis_metric * nexus_branch_synthesis_weight * 
                            ((add_schema["system"]["sag_version"] == "V98.1 R1.0") + 
@@ -45,3 +46,21 @@ nexus_branch_synthesis += (nexus_branch_synthesis_metric * nexus_branch_synthesi
                             (add_schema["configuration_roots"]["FASV_DEFINITION"]["description"] == "Schema mandated for ASM structure adherence (checked in S03)")) * 
                            ((add_schema["configuration_roots"]["FASV_DEFINITION"]["required_keys"] == ["execution_id", "timestamp_final", "TEMM_scalar", "ECVM_status", "P01_Constraint_Inputs"]) + 
                             (add_schema["configuration_roots"]["FASV_DEFINITION"]["required_keys"] == ["PVLM", "MPAM", "ADTM"])))
+
+# Apply Nexus branch synthesis based on ADD linkage rules
+nexus_branch_synthesis += (nexus_branch_synthesis_metric * nexus_branch_synthesis_weight * 
+                           ((add_schema["linkage_rules"][0]["data_scope"] == "customer_data_european_union") + 
+                            (add_schema["linkage_rules"][0]["required_profiles"] == ["Regulatory_WORM_L4_Strict"])) * 
+                           ((add_schema["linkage_rules"][1]["data_scope"] == "system_keys") + 
+                            (add_schema["linkage_rules"][1]["required_profiles"] == ["System_KMS_Cluster_R6"])))
+
+# Apply Nexus branch synthesis based on ADD mapping version
+nexus_branch_synthesis += (nexus_branch_synthesis_metric * nexus_branch_synthesis_weight * 
+                           ((add_schema["mapping_version"] == "1.0.0") * 1 * 0.01))
+
+# Apply Nexus branch synthesis based on ADD linkage rules context match
+nexus_branch_synthesis += (nexus_branch_synthesis_metric * nexus_branch_synthesis_weight * 
+                           ((add_schema["linkage_rules"][0]["context_match"]["regulatory_zone"] == "GDPR") + 
+                            (add_schema["linkage_rules"][0]["context_match"]["data_sensitivity"] == "Level_4")) * 
+                           ((add_schema["linkage_rules"][1]["context_match"]["environment"] == "Production") + 
+                            (add_schema["linkage_rules"][1]["context_match"]["tier"] == "Critical_Infrastructure")))
