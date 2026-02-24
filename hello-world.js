f58fa6f63a1595f81e0d81c8fa99d923d6aad50c
Craig Huckerby <craighckby@gmail.com>
2026-02-24 10:26:50 +1000

    Siphon Evolution [cpython]: Doc/includes/mp_workers.py

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,49 @@
+import time
+import random
+
+from multiprocessing import Process, Queue, current_process, freeze_support
 from typing import Any, Dict, List, Optional, Union
 
-# Assuming this decorator is provided by the environment
-# @_add_interface_methods
+# Dummy decorator since its definition is not provided in the prompt
+def _add_interface_methods(cls):
+    return cls
+
+#
+# Function run by worker processes
+#
+
+def worker(input_queue, output_queue):
+    for func, args in iter(input_queue.get, 'STOP'):
+        result = calculate_target_task(func, args)
+        output_queue.put(result)
+
+#
+# Function used to calculate result for TARGET objects
+#
+
+def calculate_target_task(func, args):
+    # args here would likely contain one or more TARGET objects
+    result = func(*args)
+    return '%s processed: %s%s = %s' % \
+        (current_process().name, func.__name__, args, result)
+
+#
+# Functions referenced by tasks (operating on TARGET objects)
+#
+
+def get_container_name(options: 'ContainerPluginOptions'):
+    time.sleep(0.1 * random.random()) # Simulate work
+    return options.name
+
+def get_exposes_count(options: 'ContainerPluginOptions'):
+    time.sleep(0.1 * random.random()) # Simulate work
+    exposes = options.exposes
+    if isinstance(exposes, list):
+        return len(exposes)
+    elif isinstance(exposes, dict): # ExposesObject inherits from dict
+        return len(exposes)
+    return 0 # Should not happen based on type hints
+
 
 @_add_interface_methods
 class LibraryCustomUmdCommentObject:
@@ -214,3 +256,93 @@ def create_container_plugin_options(**kwargs: Any) -> ContainerPluginOptions:
         raise TypeError(f"ContainerPluginOptions got unexpected arguments: {', '.join(kwargs.keys())}")
 
     return ContainerPluginOptions(**known_args)
+
+
+def test_target_with_multiprocessing():
+    NUMBER_OF_PROCESSES = 4
+    
+    # Create some example TARGET objects as tasks
+    # Example 1: Basic ContainerPluginOptions
+    options1 = create_container_plugin_options(
+        name="App1",
+        exposes=["./src/entry1"],
+        filename="remoteEntry.js"
+    )
+    # Example 2: ContainerPluginOptions with complex exposes
+    options2 = create_container_plugin_options(
+        name="App2",
+        exposes=create_exposes_object(
+            mod1=create_exposes_config(_import="./src/mod1", name="Module1"),
+            mod2="./src/mod2"
+        ),
+        library=create_library_options(_type="commonjs", name="MyLib"),
+        runtime="webpack-runtime"
+    )
+    # Example 3: Another basic one
+    options3 = create_container_plugin_options(
+        name="App3",
+        exposes=["./src/entry3"],
+        share_scope="default"
+    )
+    # Example 4: A more complex one
+    options4 = create_container_plugin_options(
+        name="App4",
+        exposes=create_exposes_object(
+            utils=create_exposes_config(_import=["./src/util1", "./src/util2"], name="MyUtils")
+        )
+    )
+
+    TASKS_CONTAINER_NAMES = [
+        (get_container_name, (options1,)),
+        (get_container_name, (options2,)),
+        (get_container_name, (options3,)),
+        (get_container_name, (options4,)),
+        (get_container_name, (options1,)), # Re-use for more tasks
+        (get_container_name, (options2,)),
+        (get_container_name, (options3,)),
+        (get_container_name, (options4,)),
+    ]
+
+    TASKS_EXPOSES_COUNTS = [
+        (get_exposes_count, (options1,)),
+        (get_exposes_count, (options2,)),
+        (get_exposes_count, (options3,)),
+        (get_exposes_count, (options4,)),
+    ]
+
+    # Create queues
+    task_queue = Queue()
+    done_queue = Queue()
+
+    # Submit first batch of tasks
+    print('Submitting tasks for container names...')
+    for task in TASKS_CONTAINER_NAMES:
+        task_queue.put(task)
+
+    # Start worker processes
+    for i in range(NUMBER_OF_PROCESSES):
+        Process(target=worker, args=(task_queue, done_queue)).start()
+
+    # Get and print results for container names
+    print('Unordered results for container names:')
+    for i in range(len(TASKS_CONTAINER_NAMES)):
+        print('\t', done_queue.get())
+
+    # Add more tasks using `put()` for exposes counts
+    print('\nSubmitting tasks for exposes counts...')
+    for task in TASKS_EXPOSES_COUNTS:
+        task_queue.put(task)
+
+    # Get and print some more results
+    print('Unordered results for exposes counts:')
+    for i in range(len(TASKS_EXPOSES_COUNTS)):
+        print('\t', done_queue.get())
+
+    # Tell child processes to stop
+    for i in range(NUMBER_OF_PROCESSES):
+        task_queue.put('STOP')
+
+
+if __name__ == '__main__':
+    freeze_support()
+    test_target_with_multiprocessing()
