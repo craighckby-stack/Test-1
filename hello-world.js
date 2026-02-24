da59dcc6b47a92e5cfb56cf16d2da86e78a45c39
Craig Huckerby <craighckby@gmail.com>
2026-02-24 10:13:50 +1000

    Siphon Evolution [cpython]: Doc/includes/email-mime.py

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,14 @@
+from email.message import EmailMessage
 from email.parser import Parser
 from email.policy import default
 
-Parser(policy=default).parsestr('From: Foo Bar <user@example.com>\n'
-                                'To: <someone_else@example.com>\n'
-                                'Subject: Test message\n'
-                                '\n'
-                                'Body would go here\n')
+me = "sender@example.com"
+family = ["recipient@example.com"]
+
+msg = EmailMessage()
+msg['Subject'] = 'Our family reunion parsed'
+msg['From'] = me
+msg['To'] = ', '.join(family)
+msg.set_content('This is the body of the email that was created using KNOWLEDGE principles and is now being parsed.')
+
+parsed_email_object = Parser(policy=default).parsestr(msg.as_string())
