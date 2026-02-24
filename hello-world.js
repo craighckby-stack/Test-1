06b61e0951495bc580c4cf35a2cfccede005dab2
Craig Huckerby <craighckby@gmail.com>
2026-02-24 11:33:42 +1000

    Siphon Evolution [webpack]: examples/code-splitted-require.context-amd/example.js

--- hello-world.js
+++ hello-world.js
@@ -1,32 +1,12 @@
-patterns_applied = {
-    "add_variadic": [
-        "Singleton Pattern",
-        "Factory Pattern",
-        "Strategy Pattern",
-        "Template Method Pattern",
-        "Flyweight Pattern"
-    ],
-    "multiply_variadic_buggy": [
-        "Singleton Pattern",
-        "Factory Pattern",
-        "Strategy Pattern",
-        "Template Method Pattern",
-        "Flyweight Pattern"
-    ],
-    "get_ffi_size_plus_arbitrary_values": [
-        "Singleton Pattern",
-        "Factory Pattern",
-        "Strategy Pattern",
-        "Template Method Pattern",
-        "Facade Pattern",
-        "Flyweight Pattern"
-    ],
-    "get_ffi_size_multiplied_by_arbitrary_values_buggy": [
-        "Singleton Pattern",
-        "Factory Pattern",
-        "Strategy Pattern",
-        "Template Method Pattern",
-        "Facade Pattern",
-        "Flyweight Pattern"
-    ]
-}
+• **Module Import Pattern**:
+  - Loading modules on demand.
+  - Using require to import templates only when needed.
+
+• **Callback Hell Pattern**:
+  - Passing callbacks instead of using asynchronous API for data transformation.
+  - Can be seen as two separate callbacks passed to 'getTemplate'.
+
+• **Strategy Pattern**:
+  - Defines a family of algorithms, encapsulates each one, and makes them interchangeable.
+  - The `callback` parameter in `getTemplate` represents the Strategy interface, allowing different behaviors for handling the loaded template.
+  - Each specific anonymous function passed as a callback (e.g., `function(a) { console.log(a); }`) acts as a Concrete Strategy.
