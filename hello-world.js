2d13c523236121d5cc1dd8f0a9581fdd74516f65
Craig Huckerby <craighckby@gmail.com>
2026-02-24 11:01:38 +1000

    Siphon Evolution [webpack]: examples/asset/example.js

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,9 @@
-Command/Request Pattern
+PATTERN:
+• Observer:
+  - The `createBlobElement` function uses the Observer pattern when attaching an event listener (`img.addEventListener`) to the `load` event of an image. The image acts as the subject, notifying the registered callback (observer) upon loading completion.
+
+• Strategy:
+  - The code demonstrates the Strategy pattern through the `createImageElement`, `createTextElement`, and `createBlobElement` functions, where each represents a distinct strategy for rendering content. The `render` function acts as the context, dynamically invoking the chosen strategy (passed as `fn`) to display different content types.
+
+• Dependency Injection:
+  - The `render` function utilizes Dependency Injection by accepting `fn` (which is `item.render`) as an argument. This external function, representing the rendering strategy, is injected into `render` rather than being created or resolved internally.
