234f475298a11cbf66ac4823f88a92a4e5e61934
Craig Huckerby <craighckby@gmail.com>
2026-02-25 11:11:22 +1000

    ◈ FORCE_EVOLVE ◈ Source: src/hotspot/cpu/x86/sharedRuntime_x86.cpp

--- hello-world.js
+++ hello-world.js
@@ -849,3 +849,4 @@ export default function App() {
 // SIPHON_METADATA: Evolution from src/hotspot/cpu/x86/c2_stubGenerator_x86_64_string.cpp @ 1771981692835
 // SIPHON_METADATA: Evolution from src/hotspot/cpu/x86/gc/shared/cardTableBarrierSetAssembler_x86.cpp @ 1771981757519
 // SIPHON_METADATA: Evolution from src/hotspot/cpu/x86/macroAssembler_x86.cpp @ 1771981817704
+// SIPHON_METADATA: Evolution from src/hotspot/cpu/x86/sharedRuntime_x86.cpp @ 1771981881321
