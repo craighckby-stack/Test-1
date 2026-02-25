8e6577ef45ffffffc3e22b060929b4a6a3e42b7c
Craig Huckerby <craighckby@gmail.com>
2026-02-25 10:38:46 +1000

    ◈ FORCE_EVOLVE ◈ Source: src/demo/share/jfc/Metalworks/Metalworks.java

--- hello-world.js
+++ hello-world.js
@@ -818,3 +818,4 @@ export default function App() {
 // SIPHON_METADATA: Evolution from src/demo/share/jfc/J2Ddemo/java2d/demos/Fonts/AttributedStr.java @ 1771979734356
 // SIPHON_METADATA: Evolution from src/demo/share/jfc/J2Ddemo/java2d/demos/Lines/LineAnim.java @ 1771979798669
 // SIPHON_METADATA: Evolution from src/demo/share/jfc/J2Ddemo/java2d/demos/Paths/WindingRule.java @ 1771979862072
+// SIPHON_METADATA: Evolution from src/demo/share/jfc/Metalworks/Metalworks.java @ 1771979925263
