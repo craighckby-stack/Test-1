de5903e348583f3f6af4af32071edc36537661f3
Craig Huckerby <craighckby@gmail.com>
2026-02-25 10:03:37 +1000

    ◈ FORCE_EVOLVE ◈ Source: make/ide/xcode/hotspot/src/classes/XcodeProjectMaker.java

--- hello-world.js
+++ hello-world.js
@@ -785,3 +785,4 @@ export default function App() {
 
 // SIPHON_METADATA: Evolution from make/conf/jib-profiles.js @ 1771977696043
 // SIPHON_METADATA: Evolution from make/ide/idea/langtools/template/src/idea/IdeaLoggerWrapper.java @ 1771977754544
+// SIPHON_METADATA: Evolution from make/ide/xcode/hotspot/src/classes/XcodeProjectMaker.java @ 1771977816339
